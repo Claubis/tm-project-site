9246b4b667087ed62064e888415bc878305bf0c7
Claudio Bispo <claudio_cssp@hotmail.com>
2024-04-05 12:14:09 -0300

    Ajuste do formulário de feedback

--- src/app/FormFeedback/page.tsx
+++ src/app/FormFeedback/page.tsx
@@ -7,8 +7,13 @@ export default function FormFeedback(){
 
     const [rating, setRating] = useState(0);
 
+    type StarIconProps = {
+        filled: boolean;
+        onClick: () => void;
+    };
+
     // Função para renderizar o ícone de estrela SVG
-    const StarIcon = ({ filled, onClick }) => (
+    const StarIcon = ({ filled, onClick }: StarIconProps) => (
         <svg
           onClick={onClick} // Use o onClick aqui
           className={`h-[50px] w-[80px] cursor-pointer ${filled ? 'text-yellow-500' : 'text-[#EB9AC0]'}`}
@@ -79,7 +84,7 @@ export default function FormFeedback(){
 
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
 
-                        <textarea id="message" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                        <textarea id="message" rows={8} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
 
                     </div>
 
