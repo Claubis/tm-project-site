3a28a64944596c52c35e0b170b548da89446b01b
Claudio Bispo <claudio_cssp@hotmail.com>
2024-04-07 19:24:19 -0300

    ajuste do botao send do formulario

--- src/app/FormQuote/page.tsx
+++ src/app/FormQuote/page.tsx
@@ -12,13 +12,38 @@ import imagem from '../../../public/assets/Form/imagem-removebg-preview.png';
 
 import React, { useState } from 'react';
 
+type FormDataChangeHandler = (name: string, value: string) => void;
+
+
 export default function FormQuote() {
 
     // Estado para controlar a etapa atual do formulário
   const [currentStep, setCurrentStep] = useState(1);
 
+  const [formData, setFormData] = useState({
+    firstName: '',
+    phone: '',
+    email: '',
+    streetAddress: '',
+    city: '',
+    region: '',
+    postalCode: '',
+    room: '',
+    pet: '',
+    frequency:'',
+    date: '',
+    hour:'',
+    message:''
+    
+  });
+
+  const handleFormDataChange: FormDataChangeHandler = (name, value) => {
+    setFormData(prevState => ({ ...prevState, [name]: value }));
+  };
+
   // Função para avançar para a próxima etapa
   const nextStep = () => {
+    console.log(formData);
     setCurrentStep(currentStep + 1);
   };
 
@@ -31,39 +56,57 @@ export default function FormQuote() {
   const renderStep = () => {
     switch (currentStep) {
       case 1:
-        return <PersonalInfo />;
+        return <PersonalInfo onChange={handleFormDataChange} />;
       case 2:
-        return <ResidenceInfo />;
+        return <ResidenceInfo onChange={handleFormDataChange} />;
       case 3:
-        return <AdditionalInfo />;
+        return <AdditionalInfo onChange={handleFormDataChange} />;
       case 4:
-        return <Schedule />;
+        return <Schedule onChange={handleFormDataChange} />;
       default:
-        return <PersonalInfo />;
+        return <PersonalInfo onChange={handleFormDataChange} />;
     }
   };
 
+  const handleSubmit = (event:any) => {
+    // Prevenir a submissão do formulário se não estiver na última etapa
+    if (currentStep < 4) {
+      event.preventDefault();
+      
+      nextStep();
+
+    }else {
+        console.log('Enviando o formulário...', formData);
+    }
+    
+  };
+
   return (
-    <form name='form-quote' method='post' data-netlify="true" className='bg-[#F3AACB] grid md:flex px-10 justify-center items-center min-h-[840px]'>
+    
+    <form name='form-quote' method='post' data-netlify="true" onSubmit={handleSubmit} className='bg-[#F3AACB] grid md:flex px-10 justify-center items-center min-h-[840px]'>
         
         <div className="bg-white rounded-xl h-[600px] md:h-[550px] w-[100%] md:w-[50%] p-10 order-2 md:order-1 mb-20 md:mb-0">
+        
         {renderStep()}
             <div className="flex justify-between">
             {currentStep > 1 && (
-                <button onClick={prevStep} className="mt-4 bg-gray-200 hover:bg-gray-300 text-black py-2 px-4 rounded">
+                <button type="button" onClick={prevStep} className="mt-4 bg-gray-200 hover:bg-gray-300 text-black py-2 px-4 rounded">
                 Previous
                 </button>
             )}
-            {currentStep < 4 ? (
-                <button onClick={nextStep} className="mt-4 bg-[#064E8B] hover:bg-[#B3CEE5] text-white py-2 px-4 rounded">
+            {currentStep < 4 && (
+                <button type="button" onClick={nextStep} className="mt-4 bg-[#064E8B] hover:bg-[#B3CEE5] text-white py-2 px-4 rounded">
                 Next
                 </button>
-            ) : (
+            )}
+            {currentStep === 4 && (
                 <button className="mt-4 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded" type='submit'>
                 Send
                 </button>
             )}
             </div>
+
+            
         </div>
 
         <div className="flex flex-col justify-center gap-5 mt-0 md:p-10 w-[100%] md:w-[70%] order-1">
@@ -83,7 +126,11 @@ export default function FormQuote() {
   );
 };
 
-const PersonalInfo = () => {
+interface PersonalInfoProps {
+    onChange: FormDataChangeHandler;
+  }
+
+const PersonalInfo: React.FC<PersonalInfoProps> = ({ onChange }) => {
   // Formulário de informações pessoais
   return (
     <div className="mb-5">
@@ -99,12 +146,14 @@ const PersonalInfo = () => {
                             </label>
 
                             <div className="mt-2">
+                                
                                 <input
                                 type="text"
-                                name="first-name"
-                                id="first-name"
-                                autoComplete="given-name"
+                                name="firstName"
+                                id="firstName"
+                                autoComplete="firstName"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={(e) => onChange(e.target.name, e.target.value)}
                                 />
                             </div>
 
@@ -117,12 +166,14 @@ const PersonalInfo = () => {
                             </label>
 
                             <div className="mt-2">
+                                
                                 <input
                                 type="number"
                                 name="phone"
                                 id="phone"
-                                autoComplete="phone-number"
+                                autoComplete="phone"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={(e) => onChange(e.target.name, e.target.value)}
                                 />
                             </div>
                         </div>
@@ -139,6 +190,7 @@ const PersonalInfo = () => {
                                 type="email"
                                 autoComplete="email"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={(e) => onChange(e.target.name, e.target.value)}
                                 />
 
                             </div>
@@ -150,7 +202,11 @@ const PersonalInfo = () => {
   );
 };
 
-const ResidenceInfo = () => {
+interface ResidenceInfoProps {
+    onChange: FormDataChangeHandler;
+  }
+
+const ResidenceInfo: React.FC<ResidenceInfoProps> = ({ onChange }) => {
   // Formulário de informações da residência
   return (
                     
@@ -167,12 +223,14 @@ const ResidenceInfo = () => {
                 </label>
 
                 <div className="mt-2">
+                    
                     <input
                     type="text"
-                    name="street-address"
-                    id="street-address"
-                    autoComplete="street-address"
+                    name="streetAddress"
+                    id="streetAddress"
+                    autoComplete="streetAddress"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) => onChange(e.target.name, e.target.value)}
                     />
                 </div>
 
@@ -185,12 +243,14 @@ const ResidenceInfo = () => {
                 </label>
 
                 <div className="mt-2">
+                    
                     <input
                     type="text"
                     name="city"
                     id="city"
-                    autoComplete="address-level2"
+                    autoComplete="city"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) => onChange(e.target.name, e.target.value)}
                     />
                 </div>
 
@@ -203,12 +263,14 @@ const ResidenceInfo = () => {
                 </label>
 
                 <div className="mt-2">
+                    
                     <input
                     type="text"
                     name="region"
                     id="region"
-                    autoComplete="address-level1"
+                    autoComplete="region"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) => onChange(e.target.name, e.target.value)}
                     />
                 </div>
 
@@ -221,12 +283,14 @@ const ResidenceInfo = () => {
                 </label>
 
                 <div className="mt-2">
+                    
                     <input
                     type="text"
-                    name="postal-code"
-                    id="postal-code"
-                    autoComplete="postal-code"
+                    name="postalCode"
+                    id="postalCode"
+                    autoComplete="postalCode"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) => onChange(e.target.name, e.target.value)}
                     />
                 </div>
             </div>
@@ -237,7 +301,11 @@ const ResidenceInfo = () => {
   );
 };
 
-const AdditionalInfo = () => {
+interface AdditionalInfoProps {
+    onChange: FormDataChangeHandler;
+  }
+
+const AdditionalInfo: React.FC<AdditionalInfoProps> = ({ onChange }) => {
   // Formulário de informações adicionais
   return (
     <div className="w-full">
@@ -253,11 +321,13 @@ const AdditionalInfo = () => {
                 </label>
                 
                 <div className="mt-2">
+                        
                         <select
-                        id="rooms"
-                        name="rooms"
-                        autoComplete="rooms"
+                        id="room"
+                        name="room"
+                        autoComplete="room"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                        onChange={(e) => onChange(e.target.name, e.target.value)}
                         >
                         <option>Select an option</option>
                         <option>1</option>
@@ -282,11 +352,13 @@ const AdditionalInfo = () => {
                 </label>
                 
                 <div className="mt-2">
+                    
                     <select
-                        id="pets"
-                        name="pets"
-                        autoComplete="pets"
+                        id="pet"
+                        name="pet"
+                        autoComplete="pet"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                        onChange={(e) => onChange(e.target.name, e.target.value)}
                         >
                         <option>Select an option</option>
                         <option>Yes</option>
@@ -309,6 +381,7 @@ const AdditionalInfo = () => {
                         name="frequency"
                         autoComplete="frequency"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                        onChange={(e) => onChange(e.target.name, e.target.value)}
                         >
                         <option>Select an option</option>
                         <option>Once</option>
@@ -327,7 +400,11 @@ const AdditionalInfo = () => {
   );
 };
 
-const Schedule = () => {
+interface ScheduleProps {
+    onChange: FormDataChangeHandler;
+  }
+
+const Schedule: React.FC<ScheduleProps> = ({ onChange }) => {
   // Formulário de agendamento
   return (
 
@@ -345,11 +422,13 @@ const Schedule = () => {
                 </label>
                 
                 <div className="mt-2">
+                        
                         <input type="date"
                         id="date"
                         name="date"
                         autoComplete="date"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                        onChange={(e) => onChange(e.target.name, e.target.value)}
                         ></input>
                 </div>
 
@@ -362,11 +441,13 @@ const Schedule = () => {
                 </label>
                 
                 <div className="mt-2">
+                        
                         <input type="time"
                         id="hour"
                         name="hour"
                         autoComplete="hour"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                        onChange={(e) => onChange(e.target.name, e.target.value)}
                         />
                 </div>
             </div>
@@ -375,12 +456,13 @@ const Schedule = () => {
 
                 <label className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Aditional information</label>
 
-                <textarea id="message" rows={5} className="block p-2 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                <textarea id="message" rows={5} className="block p-2 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..." onChange={(e) => onChange(e.target.name, e.target.value)}></textarea>
 
             </div>
 
         </div>
 
+    
     </div>
   );
 };
